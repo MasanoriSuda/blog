0db5ac431d21c70c48d2d99d23837a448e16fea7
MasanoriSuda <masanori.suda@gmail.com>
2025-03-07 19:51:21 +0900

    4賞まで

--- components/header.js
+++ components/header.js
@@ -1,11 +1,17 @@
 import Logo from "components/logo"
 import Nav from "components/nav"
+import styles from 'styles/header.module.css'
+import Container from "./container"
 
 export default function Header() {
     return (
         <header>
-            <Logo boxOn />
-            <Nav />
+            <Container large>
+                <div className={styles.flexContainer}>
+                    <Logo boxOn />
+                    <Nav />
+                </div>
+            </Container>
         </header>
     )
 }
---
+++ pages/blog/index.js
@@ -0,0 +1,15 @@
+import Container from "components/container";
+import Hero from "components/hero";
+
+export default function Home() {
+    return (
+        <>
+            <Container>
+                <Hero
+                    title="blog"
+                    subtitle="Recent Posts"
+                />
+            </Container>
+        </>
+    )
+}
