6341613bcb9a4a1c8b6c009a14abbe2b5a9c76b7
MasanoriSuda <masanori.suda@gmail.com>
2025-03-07 17:47:35 +0900

    wip

---
+++ components/footer.js
@@ -0,0 +1,5 @@
+import Logo from "components/logo"
+
+export default function Footer() {
+    return <footer> <Logo /> </footer >
+}
---
+++ pages/blog.js
@@ -0,0 +1,12 @@
+import Hero from "components/hero";
+
+export default function Home() {
+    return (
+        <>
+            <Hero
+                title="blog"
+                subtitle="Recent Posts"
+            />
+        </>
+    )
+}
